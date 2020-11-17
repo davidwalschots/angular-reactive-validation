94b8c586750811d6bc2519af0d626ac8d9a68c46
David Walschots <davidwalschots@users.noreply.github.com>
2020-11-17 15:28:31 +0100

    TypeScript null detection obscurity
    Even though control.parent is checked for having a value, it is still
    flagged as potentially null. Fixed by capturing the variable separately
    so TS detects it as having a value within the closure.

--- angular-reactive-validation/src/get-control-path.ts
+++ angular-reactive-validation/src/get-control-path.ts
@@ -3,18 +3,19 @@ import { AbstractControl } from '@angular/forms';
 /**
  * Given a control, returns a string representation of the property path to
  * this control. Thus, for a FormControl 'firstName', that is part of a
- * FormGroup references to as 'name', this function will return: 'name.firstName'.
+ * FormGroup named 'name', this function will return: 'name.firstName'.
  *
  * Note that FormArray indexes are also put in the path, e.g.: 'person.0.name.firstName'.
  */
 export function getControlPath(control: AbstractControl): string {
-  if (control.parent) {
-    let path = getControlPath(control.parent);
+  const parentControl = control.parent;
+  if (parentControl) {
+    let path = getControlPath(parentControl);
     if (path) {
       path += '.';
     }
-    return path + Object.keys(control.parent.controls).find(key => {
-      const controls = control.parent.controls;
+    return path + Object.keys(parentControl.controls).find(key => {
+      const controls = parentControl.controls;
       if (Array.isArray(controls)) {
         return controls[Number(key)] === control;
       } else {
