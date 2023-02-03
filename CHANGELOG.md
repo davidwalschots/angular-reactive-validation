824e26ef83e2e0310a3551cb79ed6a5dc478b6db
Jason Finch <jason.finch@gmail.com>
2023-02-03 15:07:02 +1000

    feat: Update to build against Angular 15

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 9.0.0
+
+* Add support for Angular 15
+
 ## 8.0.0
 
-* add support for Angular 14
+* Add support for Angular 14
--- test.ts
+++ test.ts
@@ -7,19 +7,8 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: any;
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
-// Then we find all the tests of the app
-const contextApp = require.context('./src', true, /\.spec\.ts$/);
-// And load the modules.
-contextApp.keys().map(contextApp);
-
-// Then we find all the tests of the lib
-const contextLib = require.context('./angular-reactive-validation', true, /\.spec\.ts$/);
-// And load the modules.
-contextLib.keys().map(contextLib);
