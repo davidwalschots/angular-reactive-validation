1484c930a21fb6b00c58eadeb1a4f134243411bd
Sami Varis <33018214+samiv22@users.noreply.github.com>
2024-01-16 22:11:16 +0200

    Update app.module.ts
    Revert ReactiveValidationModule import path

--- projects/test-app/src/app/app.module.ts
+++ projects/test-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ReactiveValidationModule } from '../../../angular-reactive-validation/src/lib/reactive-validation.module';
+import { ReactiveValidationModule } from 'angular-reactive-validation';
 
 import { AppComponent } from './app.component';
 
