ba1191b5959bac4c38737f3e18cb0881b33e8a5b
Sami Varis <samivaris95@gmail.com>
2024-01-16 18:44:25 +0200

    Angular 16 update

--- projects/test-app/src/app/app.module.ts
+++ projects/test-app/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ReactiveValidationModule } from 'angular-reactive-validation';
-
+import { ReactiveValidationModule } from '../../../angular-reactive-validation/src/lib/reactive-validation.module';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
