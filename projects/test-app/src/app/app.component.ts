323098ebc2bb322871b480a616593cd525c23ce0
Sami Varis <samivaris95@gmail.com>
2024-01-16 22:14:47 +0200

    Revert PHPStorm error 'implicitly has an any type'

--- projects/test-app/src/app/app.component.ts
+++ projects/test-app/src/app/app.component.ts
@@ -15,15 +15,15 @@ export class AppComponent {
     name: this.fb.group({
       firstName: ['', [Validators.required('A first name is required'),
         Validators.minLength(5),
-        Validators.maxLength(10, ((maxLength: any) => `Maximum length is ${maxLength}`))]],
-      middleName: ['', [Validators.maxLength(50, ((maxLength: any) => `Maximum length is ${maxLength}`))]],
+        Validators.maxLength(10, (maxLength => `Maximum length is ${maxLength}`))]],
+      middleName: ['', [Validators.maxLength(50, (maxLength => `Maximum length is ${maxLength}`))]],
       lastName: ['', [Validators.required('A last name is required'),
-        Validators.maxLength(() => 10 * 5, ((maxLength: any) => `Maximum length is ${maxLength}`))]]
+        Validators.maxLength(() => 10 * 5, (maxLength => `Maximum length is ${maxLength}`))]]
     }),
     age: [null, [
       Validators.required('An age is required'),
       Validators.min(0, `You can't be less than zero years old.`),
-      Validators.max(150, ((max: any) => `Can't be more than ${max}`))
+      Validators.max(150, (max => `Can't be more than ${max}`))
     ]]
   });
 
